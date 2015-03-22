9fb7189475b5a49af8372b2ce9c66f0426349f5a
Matthew <mchi112@aucklanduni.ac.nz>
2015-03-23 11:26:22 +1300

    First commit

---
+++ GitHubApiPrototype/GitHubApi.ts
@@ -0,0 +1,33 @@
+﻿///<reference path="Scripts/typings/jquery/jquery.d.ts"/>
+
+class GitHubApi
+{
+    private baseUrl: string;
+
+    constructor()
+    {
+        this.baseUrl = "https://api.github.com";
+    }
+
+    public GetRepos(username: string, success: any, error: any)
+    {
+        $.ajax({
+            url: this.baseUrl + "/users/" + username + "/repos",
+            dataType: "jsonp",
+            method: "GET",
+            success: success,
+            error: error
+        });
+    }
+
+    public GetPullRequests(username: string, repo: string, success: any, error: any)
+    {
+        $.ajax({
+            url: this.baseUrl + "/repos/" + username + "/" + repo + "/pulls",
+            dataType: "jsonp",
+            method: "GET",
+            success: success,
+            error: error
+        });
+    }
+}
---
+++ GitHubApiPrototype/app.ts
@@ -0,0 +1,31 @@
+﻿/// <reference path="./GitHubApi.ts"/>
+
+window.onload = () => {
+    var api = new GitHubApi();
+
+    $("#getrepoform").click(function (event) {
+        api.GetRepos($("#getrepousername").val(), successGetRepo, error);
+    });
+
+    $("#getprform").click(function (event) {
+        api.GetPullRequests($("#getprusername").val(), $("#getprrepo").val(), successPullReq, error);
+    });
+};
+
+function successGetRepo(data: any): void {
+    console.log("GET Repositories success");
+    for (var i = 0; i < data.data.length; i++) {
+        console.log(i + ": " + data.data[i].full_name);
+    }
+}
+
+function successPullReq(data: any): void {
+    console.log("GET Pull Requests success");
+    for (var i = 0; i < data.data.length; i++) {
+        console.log(i + ": " + data.data[i].body);
+    }
+}
+
+function error(): void {
+    alert("Request failed!");
+}
